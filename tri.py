7d7a7557303255dc99f6f2a922f5042b225e5705
yashmali408 <yashmali408@gmail.com>
2023-12-06 18:35:29 +0530

    yyy

---
+++ tri.py
@@ -0,0 +1,7 @@
+b = input("Place the vloume of Bright here")
+b = int(b)
+h = input("Place the volume of Height here")
+h = int(h)
+
+ answer=(b*h)/2
+print(f"The area of traingle is{answer}")      
